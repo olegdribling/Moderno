6f2a572be14e6b5495bcac4ca50f09bac8884aca
olegdribling <o.efendiev@gmail.com>
2020-04-09 11:36:40 +0300

    commi

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    $('.product-slider__inner').slick({
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      arrows: false,
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
